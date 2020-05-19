6616eea3ce9737eb8b72aebe6e1d4d21c90d6210
Matheus Izidoro <matheusizidoro07@gmail.com>
2020-05-19 10:07:14 -0300

    obstacles reset working

---
+++ Assets/Scripts/ObstacleBehaviour.cs
@@ -0,0 +1,44 @@
+﻿using Assets.Scripts;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ObstacleBehaviour : MonoBehaviour
+{
+    public Rigidbody rb;
+
+    Vector3 initPos;
+    Vector3 initRot;
+
+    private bool _reseting;
+    public bool Reseting
+    {
+        get { return _reseting; }
+        set { _reseting = value; }
+    }
+
+    public void Reset()
+    {
+        transform.position = initPos;
+        transform.eulerAngles = initRot;
+
+        Reseting = false;
+    }
+
+    void Start()
+    {
+        initPos = transform.position;
+        initRot = transform.eulerAngles;        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {  
+        if(GameControl.Instance.GameState != GameState.Finished && Reseting)
+        {
+            Reset();
+
+            Reseting = false;
+        }
+    }
+}
